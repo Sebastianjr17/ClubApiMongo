2f196119c4630ddd1adff594c37cfef8285a97a6
Sebastianjr17 <sebastiianjr94@gmai.com>
2024-10-05 10:18:51 -0500

    ALV

--- src/main/java/com/ClubApiMongo/app/controller/HomeTemplateController.java
+++ src/main/java/com/ClubApiMongo/app/controller/HomeTemplateController.java
@@ -1,16 +1,13 @@
 package com.ClubApiMongo.app.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/home")
 public class HomeTemplateController {
 
-    @GetMapping("/")
-    public String jugadorListTemplate(Model model) {
-        return "home";
-    }
+	@GetMapping("/index")
+	public String index() {
+		return "index";
+	}
 }
